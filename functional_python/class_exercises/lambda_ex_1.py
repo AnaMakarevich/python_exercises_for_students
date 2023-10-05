39c972b8b6e594edae3f07f5b74ee84bbdb41d36
Ana.Makarevich <electric.blake@gmail.com>
2023-10-05 20:40:09 +0200

    Fix import

--- functional_python/class_exercises/lambda_ex_1.py
+++ functional_python/class_exercises/lambda_ex_1.py
@@ -1,7 +1,7 @@
-from class_exercises.utils import test_my_solution
+from utils import test_my_solution
 
 
-def apply_to_each_element(numbers: list, f: callable) -> list:
+def apply_to_each_element(numbers: list, f: callable) -> list | None:
     if f == "TODO":
         return None
     else:
@@ -10,10 +10,8 @@ def apply_to_each_element(numbers: list, f: callable) -> list:
             result.append(f(element))
         return result
 
-    # Завдання: замініть TODO лямбда-функцією
-
 
 source_array = [1, 2, 3, 4, 5]
 expected_result = [6, 7, 8, 9, 10]
-actual_result = apply_to_each_element([1, 2, 3, 4, 5], "TODO")  # Замініть dummy своєю реалізацією
+actual_result = apply_to_each_element(source_array, "TODO")  # Замініть dummy своєю реалізацією
 test_my_solution(actual_result, expected_result)
